48bb3d5ef4399395030102845660fbdb227953ad
toetra <rabotosontoetra@yahoo.fr>
2021-01-19 18:02:01 +0300

    commit farany

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Comment from './component_2/Comment.js';
+/*import Comment from './component_2/Comment.js';*/
+import Clock from './component_2/Clock.js';
 
 function App() {
+  /* ceci est pour le Composants et props */
+/*
 	var author = 	{
 						avatarUrl:"images/toetra.png", 
-						name:"Mampitoetra"
+						name:"Mampitoetra Rabotoson"
 					};
 	var text = "Bonjour à tous, Nous sommes à la blabla"
   	return (
@@ -14,6 +17,14 @@ function App() {
       		<Comment author={author} text={text} />
     	</div>
   	);
+*/
+  	
+    return (
+      <div className="App">
+          <Clock  />
+      </div>
+    );
 }
 
 export default App;
+
