4f323568c08a2408b6f423ed00e64902ed374093
Christophe RAZAFIMAHATRATRA <rhisty@gmail.com>
2021-01-25 09:00:53 +0300

    Functionnal component

--- src/component_2/Comment_2.js
+++ src/component_2/Comment_2.js
@@ -1,52 +1,31 @@
 import React from 'react';
 import Comment from './Comment';
 
-class Comment_2 extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-          isLoaded: false,
-          items: []
-        };
-  }
+function Comment_2() {
+    const [loading, setLoading] = React.useState(false);
+    const [items, setItems] = React.useState([]);
+    React.useEffect(() => {
+        setLoading(false);
+        fetch("http://localhost:4000/posts").then(res => res.json()).then(result => {
+            setLoading(false);
+            setItems(result);
+        })
+    }, []);
 
-  componentDidMount() {
-    this.getLists();
-  }
-  getLists() {
-    this.setState({ loading: true }, () => {
-      fetch("http://localhost:4000/posts")
-        .then(res => res.json())
-        .then(result =>
-          this.setState({
-            isLoaded: true,
-            items: result
-          })
-        )
-        .catch(console.log);
-    });
-  }
-  render() {
-    var {isLoaded, items} = this.state;
-    //console.log(items);
-    if (!isLoaded) {
-      return <div>Loading...</div>;
-    }
-    else{
-      return ( 
-          
-            <div className="row" >
-                
+    return (
+        <>
+            {loading && <div>Loading...</div>}
+            {!loading &&
+                <div className="row" >
                     <div className="col-sm-12" >
-                        {items.map((item, i)=>(
-                          <Comment key={`bouton-${i}`} user={item} />
+                        {items.map((item, i) => (
+                            <Comment key={`bouton-${i}`} user={item} />
                         ))}
                     </div>
-            </div>
-      );
-      
-    }
-  }
+                </div>
+            }
+        </>
+    )
 }
 
 export default Comment_2;
